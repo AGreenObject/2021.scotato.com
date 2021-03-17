1cb060c686e7dcbb27359af3fed09536305135c5
Scott Dodge <scott@goldilocks.design>
2021-03-16 22:22:54 -0500

    remove boilerplate, add socials

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faGithub,
+  faFigma,
+  faEthereum,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a href="https://github.com/scotato">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+
+      <a href="https://www.figma.com/@scotato">
+        <FontAwesomeIcon icon={faFigma} />
+      </a>
+
+      <a href="https://twitter.com/scotato">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+
+      <a href="https://eth.af/scotato">
+        <FontAwesomeIcon icon={faEthereum} />
+      </a>
+    </>
   );
 }
 
