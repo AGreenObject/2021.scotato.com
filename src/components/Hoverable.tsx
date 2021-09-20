fff0823199ce3aedc5bc238e6c4ecfc0ea1bc014
Scott Dodge <scott@scotato.com>
2021-09-19 18:18:07 -0700

    add use-sound, add hoverable tap sounds

--- src/components/Hoverable.tsx
+++ src/components/Hoverable.tsx
@@ -1,38 +1,46 @@
-import { motion, HTMLMotionProps } from "framer-motion"
-import React from 'react'
+import React from "react";
+import { motion, HTMLMotionProps } from "framer-motion";
+import useSound from "use-sound";
+// @ts-ignore
+import tapSfx from "../sounds/primary-system-sounds/navigation_forward-selection-minimal.wav";
 
 const spring = {
-  type: 'spring',
+  type: "spring",
   damping: 15,
   stiffness: 500,
   restSpeed: 0.001,
-  restDelta: 0.001
-}
+  restDelta: 0.001,
+};
 
 const variants = {
   visible: {
     y: 0,
     opacity: 1,
-    transition: { y: spring }
+    transition: { y: spring },
   },
   hidden: {
     y: 8,
     opacity: 0,
-    transition: { y: spring }
-  }
-}
+    transition: { y: spring },
+  },
+};
 
-const Hoverable = React.forwardRef<HTMLDivElement, HTMLMotionProps<"div">>((props, ref) => {
-  return (
-    <motion.div
-      whileHover={{ scale: 1.02 }}
-      whileTap={{ scale: 0.99 }}
-      variants={variants}
-      transition={spring}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+const Hoverable = React.forwardRef<HTMLDivElement, HTMLMotionProps<"div">>(
+  (props, ref) => {
+    const [playTap] = useSound(tapSfx);
+
+    return (
+      <motion.div
+        whileHover={{ scale: 1.02 }}
+        whileTap={{ scale: 0.99 }}
+        variants={variants}
+        transition={spring}
+        onMouseDown={() => playTap()}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Hoverable;
